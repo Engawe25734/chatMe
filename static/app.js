5850b8d93e151beb7a5cf83206495b15d0b418ff
Engawe25734 <95743057+Engawe25734@users.noreply.github.com>
2026-07-28 17:13:45 -0400

    Add savePrivacy function for updating privacy settings
    Implemented savePrivacy function to update user privacy settings via API.

--- static/app.js
+++ static/app.js
@@ -499,14 +499,72 @@ function openChat(){
 
 }
 
+//===================================
+//save privacy
+//=====================================
+async function savePrivacy(){
 
 
+await fetch(
+
+`${API_URL}/privacy/update`,
+
+{
+
+method:"POST",
+
+headers:{
+
+"Content-Type":
+"application/json"
+
+},
 
 
+body:JSON.stringify({
+
+username:username,
+
+last_seen:
+document.getElementById(
+"last-seen"
+).value,
 
 
+online:
+document.getElementById(
+"online-status"
+).value,
 
 
+photo:
+document.getElementById(
+"photo-privacy"
+).value,
+
+
+read_receipts:
+document.getElementById(
+"read-receipts"
+).checked ? 1:0,
+
+
+bio:"everyone"
+
+
+})
+
+}
+
+);
+
+
+alert(
+"Privacy settings saved"
+);
+
+
+}
 // =====================================
 // WEBSOCKET CONNECTION
 // Matches server.py
@@ -523,9 +581,7 @@ function connectSocket(){
     async def websocket_endpoint(
        websocket:WebSocket,
        username:str
-):`
-
-    );
+);
 
 
 
@@ -2568,10 +2624,6 @@ function saveAppearance(){
 
 
 }
-
-
-
-
 
 
 // =====================================
